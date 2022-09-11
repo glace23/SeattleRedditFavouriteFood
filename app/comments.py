2cffea36fd35e9934d2131632c0d1f62898774af
Glace23 <vikash23galce@gmail.com>
2022-09-11 16:46:33 -0700

    Update gmaps location checking

--- app/comments.py
+++ app/comments.py
@@ -4,26 +4,21 @@ from praw.models import MoreComments
 
 url = "https://www.reddit.com/r/SeattleWA/comments/xa3qnl/what_is_your_favourite_restaurant_in_seattle/"
 submission = api_key.reddit_read_only.submission(url=url)
-print(submission.title)
 
-comments = submission.comments.list()
 post_comments = []
+submission.comments.replace_more()
 
-for comment in comments:
-    if type(comment) == MoreComments:
-        continue
-
+for comment in submission.comments:
     post_comments.append([comment.body, comment.score])
 
+print(len(post_comments))
 # creating a dataframe
 comments_df = pd.DataFrame(post_comments, columns=['comment', 'score'])
 
-f = open("comments.txt", "w")
+f = open("comments.csv", "w")
 for comment, score in comments_df.values:
-    print(comment)
-    comment_encode = comment.encode("ascii", "ignore")
-    comment_decode = comment_encode.decode()
-    f.write(f"{comment_decode}\n")
-    #f.write(f"{comment}\n")
+    comment_encode = comment.encode("gbk", "ignore")
+    comment_decode = comment_encode.decode("gbk")
+    f.write(f"{comment_decode.replace(',', '.')}, {score}\n")
 f.close()
 
--- app/gmaps_location.py
+++ app/gmaps_location.py
@@ -2,23 +2,35 @@ import googlemaps
 import api_key
 
 gmaps = googlemaps.Client(key=api_key.google_maps_api)
-# location_bias = "circle:50000@47.618266,-122.314647"
+name = "tianfu"
 location = "47.618266,-122.314647"
-radius = 10000
+radius = 25000
 
-places_result = gmaps.places(query="Queen Cà Phê",
+places_result = gmaps.places(query=name,
                              location=location,
                              radius=radius,
                              )
 
 print(places_result)
 
-for i in places_result['results']:
-    print(i)
+place_result_length = min(2, len(places_result['results']))
+processed_places = []
+print(len(places_result['results']))
 
-# candidate_list = places_result["candidates"]
-# place_id = candidate_list[0]["place_id"]
-#
-# reverse_geocode_result = gmaps.reverse_geocode(latlng=place_id)
-#
-# print(reverse_geocode_result)
+for i in range(place_result_length):
+    if 'food' in set(places_result['results'][i]['types']) or 'restaurant' in set(places_result['results'][i]['types']):
+        name_encode = places_result['results'][i]['name'].encode("utf-8", "ignore")
+        name_decode = name_encode.decode("utf-8")
+        processed_places.append({
+            'location': places_result['results'][i]['geometry']['location'],
+            'name': name,
+            'place_id': places_result['results'][i]['place_id']
+        })
+
+print(processed_places)
+
+f = open("places.csv", "w")
+f.write(f"WKT, name, description\n")
+for place in processed_places:
+    f.write(f"POINT({place['location']['lng']} {place['location']['lat']}), {place['name'].capitalize()}, {10}\n")
+f.close()
